2d8db65b7ebcd59f656fd1b5968c1d757bbbc3a8
kstaff123 <kstaffreal@gmail.com>
2023-05-07 11:12:05 -0700

    fixed hardcoded file path

--- dashboard.js
+++ dashboard.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
         newSlide.innerHTML = `
             <div class="flex justify-center space-x-6 overflow-x-auto py-10">
                 <div class="bg-white shadow-md rounded-lg p-4 flex flex-col items-center w-48">
-                    <img src="C:\\dev\\League Login Manager/rankicons/${rank}.png" alt="Rank Badge" class="w-24 h-24">
+                    <img src="/rankicons/${rank}.png" alt="Rank Badge" class="w-24 h-24">
                     <h2 class="text-xl font-semibold mt-2">${username}</h2>
                     <button class="bg-blue-500 text-white px-4 py-2 mt-4 rounded" onclick="launchAndLogin('${username}', '${password}')">Login</button>
                 </div>
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     emptySlide.innerHTML = `
                         <div class="flex justify-center space-x-6 overflow-x-auto py-10">
                             <div class="bg-white shadow-md rounded-lg p-4 flex flex-col items-center w-48">
-                                <img src="C:\\dev\\League Login Manager/rankicons/${rank}.png" alt="Rank Badge" class="w-24 h-24">
+                                <img src="/rankicons/${rank}.png" alt="Rank Badge" class="w-24 h-24">
                                 <h2 class="text-xl font-semibold mt-2">${username}</h2>
                                 <button class="bg-blue-500 text-white px-4 py-2 mt-4 rounded" onclick="launchAndLogin('${username}', '${password}')">Login</button>
                             </div>
